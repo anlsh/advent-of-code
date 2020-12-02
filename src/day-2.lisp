1c27d365f8f713f356cab5d963aed855bf40d0c7
Anish Moorthy <anlsh@protonmail.com>
2020-12-02 13:00:22 -0500

    Clean up solution to day 2 based on reddit advice
    I haven't actually tested it though, because of working directory issues

--- src/day-2.lisp
+++ src/day-2.lisp
@@ -1,13 +1,11 @@
 (uiop:define-package :advent/src/day-2
   (:use #:cl #:iterate)
+  (:local-nicknames (#:alx #:alexandria))
   (:export
    #:solve-2a
    #:solve-2b))
 
 (in-package :advent/src/day-2)
-
-;; The (length (filter ...)) pattern involves an extra pass over the list. Iterate probably has some
-;; clause or the other which will take care of that in a jiffy though
 
 (defclass pwd-entry ()
   ((min :initarg :min)
@@ -18,29 +16,22 @@
 (defun get-password-entries ()
   (iter (for line in-file "inputs/2.txt" using #'read-line)
     (until (zerop (length line)))
-    (collect (ppcre:register-groups-bind (min-str max-str letter-str pwd)
+    (collect
+        (ppcre:register-groups-bind
+            ((#'parse-integer min) (#'parse-integer max) letter-str pwd)
                  ("(\\d+)-(\\d+) (.): (.*)" line)
-               (make-instance 'pwd-entry
-                              :min (parse-integer min-str)
-                              :max (parse-integer max-str)
-                              :letter (elt letter-str 0)
-                              :pwd pwd)))))
+               (make-instance 'pwd-entry :min min :max max
+                              :letter (elt letter-str 0) :pwd pwd)))))
 
 (defun solve-2a ()
-  (length (remove-if-not
-           (lambda (pwd-entry)
-             (with-slots (min max letter pwd) pwd-entry
-               (<= min (count letter pwd) max)))
-           (get-password-entries))))
-
-(defun xor (a b)
-  (and (or a b)
-       (not (and a b))))
+  (count-if (lambda (pwd-entry)
+              (with-slots (min max letter pwd) pwd-entry
+                (<= min (count letter pwd) max)))
+            (get-password-entries)))
 
 (defun solve-2b ()
-  (length (remove-if-not
-           (lambda (pwd-entry)
-             (with-slots (min max letter pwd) pwd-entry
-               (xor (equalp letter (elt pwd (1- min)))
-                    (equalp letter (elt pwd (1- max))))))
-           (get-password-entries))))
+  (count-if (lambda (pwd-entry)
+              (with-slots (min max letter pwd) pwd-entry
+                (alx:xor (equalp letter (elt pwd (1- min)))
+                         (equalp letter (elt pwd (1- max))))))
+            (get-password-entries)))
