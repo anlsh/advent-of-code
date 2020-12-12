94da44d0b2964e3d770b915898ab4dfa8cb28841
Anish Moorthy <anlsh@protonmail.com>
2020-12-12 14:39:56 -0500

    Simplify 12

--- src/day-12.lisp
+++ src/day-12.lisp
@@ -14,52 +14,39 @@
 (defun get-inputs ()
   (mapcar (lambda (str)
             (ppcre:register-groups-bind
-                (((lambda (char) (elt char 0)) dir) (#'parse-integer dist))
-                ("(N|S|E|W|F|L|R)([0-9]*)$" str)
+                (dir (#'parse-integer dist))
+                ("(.)(.*)$" str)
               (list dir dist)))
           (uiop:read-file-lines "../inputs/12.txt")))
 
-(defun cardinal-to-dir (card)
-  (ecase card
-    (#\N #C(0 1))
-    (#\S #C(0 -1))
-    (#\E #C(1 0))
-    (#\W #C(-1 0))))
+(defvar *card-to-vel*
+  (fset:map ("N" #C(0 1))
+            ("S" #C(0 -1))
+            ("E" #C(1 0))
+            ("W" #C(-1 0))))
 
-(defun angle-to-dir (angle)
-  (ecase (mod angle 360)
-    (90 #C(0 1))
-    (270 #C(0 -1))
-    (0 #C(1 0))
-    (180 #C(-1 0))))
+(defvar *dir-to-vel*
+  (fset:map ("L" #C(0 1))
+            ("R" #C(0 -1))))
 
 (defun day-12a ()
-  (loop with angle = 0
+  (loop with vel = #C(1 0)
         with pos = #C(0 0)
-        for (mdir mdist) in (get-inputs) do
-          (cond ((find mdir "NSEW") (incf pos (* mdist (cardinal-to-dir mdir))))
-                ((equalp mdir #\F) (incf pos (* mdist (angle-to-dir angle))))
-                (t (incf angle (* mdist (if (equalp mdir #\L) 1 -1)))))
+        for (letter num) in (get-inputs) do
+          (cond ((str:contains? letter "NSEW") (incf pos (* num (fset:@ *card-to-vel* letter))))
+                ((equalp letter "F") (incf pos (* num vel)))
+                (t (setf vel (* vel (expt (fset:@ *dir-to-vel* letter) (/ num 90))))))
         finally
            (return (+ (abs (realpart pos))
                       (abs (imagpart pos))))))
 
-(defun rotate (waypoint angle)
-  (let ((r (realpart waypoint))
-        (i (imagpart waypoint)))
-    (ecase (mod angle 360)
-      (90 (complex (* -1 i) r))
-      (270 (complex i (* -1 r)))
-      (0 waypoint)
-      (180 (complex (* -1 r) (* -1 i))))))
-
 (defun day-12b ()
   (loop with waypoint = #C(10 1)
         with pos = #C(0 0)
-        for (mdir mdist) in (get-inputs) do
-          (cond ((find mdir "NSEW") (incf waypoint (* mdist (cardinal-to-dir mdir))))
-                ((equalp mdir #\F) (incf pos (* mdist waypoint)))
-                (t (setf waypoint (rotate waypoint (* mdist (if (equalp mdir #\L) 1 -1))))))
+        for (letter num) in (get-inputs) do
+          (cond ((str:contains? letter "NSEW") (incf waypoint (* num (fset:@ *card-to-vel* letter))))
+                ((equalp letter "F") (incf pos (* num waypoint)))
+                (t (setf waypoint (* waypoint (expt (fset:@ *dir-to-vel* letter) (/ num 90))))))
         finally
            (return (+ (abs (realpart pos))
                       (abs (imagpart pos))))))
