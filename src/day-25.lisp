2011fac7c8b7389ab2465688fd19664322c68e98
Anish Moorthy <anlsh@protonmail.com>
2020-12-25 02:39:07 -0500

    Solve day 25

---
+++ src/day-25.lisp
@@ -0,0 +1,24 @@
+
+(uiop:define-package :advent/src/day-25
+  (:use #:cl #:arrow-macros #:iterate)
+  (:export
+   #:day-25))
+
+(in-package :advent/src/day-25)
+
+(defparameter *subject-num* 7)
+(defparameter *mod-base* 20201227)
+
+(defun modlog (remainder)
+  (loop for num = 1 then (mod (* num *subject-num*) *mod-base*)
+        for i from 0
+        when (= num remainder)
+          do (return i)))
+
+(defun modexpt (subject-num pwr)
+  (loop for n = 1 then (mod (* n subject-num) *mod-base*)
+        for i below pwr
+        finally (return n)))
+
+(defun solve-25 ()
+  (modexpt 16915772 (modlog 18447943)))
