8efc870832c0af5fd8ffff0871c4b3df5e73d708
Anish Moorthy <anlsh@protonmail.com>
2021-12-01 16:44:10 -0500

    2021 Day 1

--- 2021/src/day-01.lisp
+++ 2021/src/day-01.lisp
@@ -5,5 +5,21 @@
    #:solve-1a
    #:solve-1b))
 
+(in-package :advent/2021/src/day-01)
+
+(defun count-increases (ls)
+  (let ((vector (make-array (length ls) :initial-contents ls)))
+    (loop for i from 1 below (array-total-size vector)
+          counting (> (aref vector i) (aref vector (1- i))))))
+
 (defun solve-1a ()
-  (length (uiop:read-file-lines "../inputs/21.txt")))
+  (-<> (uiop:read-file-lines "../inputs/01.txt")
+    (mapcar #'parse-integer <>)
+    (count-increases <>)))
+
+(defun solve-1b ()
+  (-<> (uiop:read-file-lines "../inputs/01.txt")
+    (mapcar #'parse-integer <>)
+    (list <> (cdr <>) (cddr <>))
+    (apply #'mapcar #'+ <>)
+    (count-increases <>)))
