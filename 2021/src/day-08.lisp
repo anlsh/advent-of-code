31d40ed58588ecb11cf75ca322129747830dbb07
Anish Moorthy <anlsh@protonmail.com>
2021-12-08 02:22:53 -0500

    Fuck

--- 2021/src/day-08.lisp
+++ 2021/src/day-08.lisp
@@ -7,5 +7,74 @@
 
 (in-package :advent/2021/src/day-08)
 
+(defun string-to-set (string)
+  (fset:convert 'fset:set string))
+
 (defparameter *input*
-  (uiop:read-file-lines "../inputs/07.txt"))
+  (-<> (uiop:read-file-lines "../inputs/08.example")
+    (mapcar (lambda (line)
+              (let ((splits (str:split " | " line)))
+                (list (mapcar #'string-to-set (str:split " " (car splits)))
+                      (mapcar #'string-to-set (str:split " " (cadr splits))))))
+            <>)))
+
+
+(defparameter *num-to-codes*
+  (fset:map
+   (0 (string-to-set "abcefg"))
+   (1 (string-to-set "cf"))
+   (2 (string-to-set "acdeg"))
+   (3 (string-to-set "acdfg"))
+   (4 (string-to-set "bcdf"))
+   (5 (string-to-set "abdfg"))
+   (6 (string-to-set "abdefg"))
+   (7 (string-to-set "acf"))
+   (8 (string-to-set "abcdefg"))
+   (9 (string-to-set "abcdfg"))))
+
+(defparameter *strlen-to-nums*
+  (let ((map (fset:empty-map (fset:empty-set))))
+    (fset:image (lambda (key val)
+                  (fset:adjoinf (fset:@ map (fset:size val)) key))
+                *num-to-codes*)
+    map))
+
+(defun get-representatives (fakechars-to-candidates)
+  (if (zerop (fset:size fakechars-to-candidates))
+      (return-from get-representatives (values (fset:empty-map) t)))
+  (let ((min-key (iter:iter
+                   (iter:for k in (fset:convert 'list (fset:domain fakechars-to-candidates)))
+                   (iter:finding k iter:minimizing (fset:size (fset:@ fakechars-to-candidates k))))))
+    (if (zerop (fset:size (fset:@ fakechars-to-candidates min-key)))
+        (return-from get-representatives (values nil nil))
+        (loop for candidate in (fset:convert 'list (fset:@ fakechars-to-candidates min-key))
+              do (multiple-value-bind
+                       (mapping valid?)
+                     (get-representatives (fset:less (fset:image (lambda (k s) (values k (fset:less s candidate)))
+                                                                 fakechars-to-candidates)
+                                                     min-key))
+                   (if valid?
+                       (return (values (fset:with mapping min-key candidate) t))
+                       (values nil nil)))
+              finally
+                 (return (values nil nil))))))
+
+(defun get-fake-to-real-mapping (scrambled-lines)
+  (let ((mapping (fset:empty-map (string-to-set "abcdefg"))))
+    (fset:image
+     (lambda (scrambled-set)
+       (fset:image (lambda (scrambled-letter)
+                     (fset:intersectf (fset:@ mapping scrambled-letter)
+                                      (fset:reduce #'fset:union
+                                                   (fset:image (lambda (key)
+                                                                 (fset:@ *num-to-codes* key))
+                                                               (fset:@ *strlen-to-nums*
+                                                                       (fset:size scrambled-set)))
+                                                   :initial-value (fset:empty-set))))
+                   scrambled-set))
+     scrambled-lines)
+    mapping))
+
+(defun solve-8b ()
+  (loop for scrambled-line in *input*
+        collect (get-fake-to-real-mapping (car scrambled-line))))
--- 2021/src/day8.py
+++ 2021/src/day8.py
@@ -7,19 +7,77 @@ def get_input(fname):
     with open(fname, "r") as f:
         lines = f.readlines()
 
+    ins = []
     outs = []
     for line in lines:
-        s = line.split(" | ")[1][:-1]
-        outs.append(s.split(" "))
-    return outs
+        s = line.split(" | ")
+        s0 = s[0].split(" ")
+        s1 = s[1][:-1].split(" ")
+        ins.append(s0)
+        outs.append(s1)
+    return ins, outs
+
+num_to_segments = {
+    1: "cf",
+    7: "acf",
+    4: "bcdf",
+    2: "acdeg",
+    3: "acdfg",
+    5: "abdfg",
+    6: "abdefg",
+    0: "abcefg",
+    9: "abcdfg",
+    8: "abcdefg",
+}
+
+segments_to_num = {}
+for key in num_to_segments:
+    segments_to_num[num_to_segments[key]] = key
+
+alphabet = "abcdefg"
+
+def make_mapping(patterns):
+    f2real = {}
+    for letter in alphabet:
+        f2real[letter] = set(alphabet)
+
+    for p in patterns:
+        if len(p) == 2:
+            for letter in p:
+                f2real[letter] = set.intersection(f2real[letter], set(num_to_segments[1]))
+        elif len(p) == 3:
+            for letter in p:
+                f2real[letter] = set.intersection(f2real[letter], set(num_to_segments[7]))
+        elif len(p) == 4:
+            for letter in p:
+                f2real[letter] = set.intersection(f2real[letter], set(num_to_segments[4]))
+        elif len(p) == 5:
+            s = set()
+            for num in [2, 3, 5]:
+                s = s.union(set(num_to_segments[num]))
+            for letter in p:
+                f2real[letter] = set.intersection(f2real[letter], s)
+        elif len(p) == 6:
+            s = set()
+            for num in [6, 0, 9]:
+                s = s.union(set(num_to_segments[num]))
+            for letter in p:
+                f2real[letter] = set.intersection(f2real[letter], s)
+        elif len(p) == 7:
+            for letter in p:
+                f2real[letter] = set.intersection(f2real[letter], set(num_to_segments[8]))
+
+    print(min(len(f2real[k]) for k in f2real))
+
+    def make_backtracking_map():
+        curr_mapping = {}
+        for a
+
 
 if __name__ == "__main__":
     inp = get_input(sys.argv[1])
 
-    s = 0
-    for outs in inp:
-        for w in outs:
-            if len(w) in [2, 4, 3, 7]:
-                s += 1
+    patterns = inp[0]
 
-    print(s)
+    for pline in patterns:
+        make_mapping(pline)
