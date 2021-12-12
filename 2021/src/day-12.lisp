360873fb6662599a5db0d99f555b7fcf55cfd91d
Anish Moorthy <anlsh@protonmail.com>
2021-12-12 02:24:13 -0500

    FUCK

--- 2021/src/day-12.lisp
+++ 2021/src/day-12.lisp
@@ -12,7 +12,7 @@
 (defparameter *nbors* (fset:empty-map (fset:empty-set)))
 
 (defun build-graph ()
-  (-<> (uiop:read-file-lines "../inputs/12.txt")
+  (-<> (uiop:read-file-lines "../inputs/12.example")
     (mapcar (lambda (str) (ppcre:register-groups-bind (start end) ("(.*)-(.*)" str)
                             (fset:adjoinf (fset:@ *nbors* start) end)
                             (fset:adjoinf (fset:@ *nbors* end) start)))
@@ -20,7 +20,7 @@
 
 (build-graph)
 
-(defun solve-12-helper (max-visit-count)
+(defun solve-12-helper (graph)
   (labels ((count-ending-paths (node visit-counts)
              (if (equalp node "end")
                  1
@@ -31,11 +31,34 @@
                                                                         visit-counts)))
                                               (fset:convert 'fset:seq (fset:filter
                                                                        (lambda (node) (if (is-lower node)
-                                                                                          (< (fset:multiplicity visit-counts node) max-visit-count)
+                                                                                          (< (fset:multiplicity visit-counts node) 1)
                                                                                           t))
-                                                                       (fset:@ *nbors* node))))
+                                                                       (fset:@ graph node))))
                               :initial-value 0))))
-    (count-ending-paths "start" (fset:bag "start" "start" "end"))))
+    (count-ending-paths "start" (fset:bag "start" "start"))))
 
 (defun solve-12a ()
-  (solve-12-helper 1))
+  (solve-12-helper *nbors*))
+
+(defparameter *nbors-p2* (fset:empty-map (fset:empty-set)))
+
+(defun inflate-node (node)
+  (if (is-lower node)
+      (fset:set node (concatenate 'string node "--dup"))
+      (fset:set node)))
+
+(defun build-graph-p2 ()
+  (labels ((add-nbors (root nbor)
+             (fset:image (lambda (root)
+                           (let ((nbors (inflate-node nbor)))
+                             (fset:image (lambda (nbor)
+                                           (fset:adjoinf (fset:@ *nbors-p2* root) nbor)
+                                           (fset:adjoinf (fset:@ *nbors-p2* nbor) root))
+                                         nbors)))
+                         (inflate-node root))))
+    (-<> (uiop:read-file-lines "../inputs/12.txt")
+      (mapcar (lambda (str) (ppcre:register-groups-bind (start end) ("(.*)-(.*)" str)
+                              (add-nbors start end)))
+              <>))))
+
+(build-graph-p2)
