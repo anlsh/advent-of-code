2c4cca9d569fdf21eba4fcdadf9f1b076e793236
Anish Moorthy <anlsh@protonmail.com>
2021-12-16 03:23:16 -0500

    foo

--- 2021/src/day16.lisp
+++ 2021/src/day16.lisp
@@ -11,13 +11,7 @@
 (defparameter *input*
   (-<> (uiop:read-file-lines "../inputs/16.txt")
     (car <>)
-    (bit-smasher:hex->bits <>)
-    ))
-
-(defun str-to-bits (str)
-  (-<> str
-    (loop for c across <> collecting (parse-integer (string c)))
-    (make-array (length <>) :element-type 'bit :initial-contents <>)))
+    (bit-smasher:hex->bits <>)))
 
 (defun parse-literal (binary ostart)
   (loop with continue? = t
@@ -29,44 +23,32 @@
            (setf continue? (= 1 (aref binary start)))
            (incf start 5)
         finally
-           (return (values (bit-smasher:bits->int bits)
-                           start
-                           ;; (* 4 (ceiling (/ start 4)))
-                           ))))
+           (return (values (bit-smasher:bits->int bits) start))))
 
 (defun parse-packet (binary start)
   (let* ((version-binary (subseq binary start (+ 3 start)))
          (id-binary (subseq binary (+ 3 start) (+ 6 start)))
          (version (bit-smasher:bits->int version-binary))
          (id (bit-smasher:bits->int id-binary)))
-    ;; (print version)
     (if (= id 4)
         (return-from parse-packet (multiple-value-bind (packet packet-end) (parse-literal binary start)
-                                    (format t "Detected literal packet in range ~a,~a w/ value ~a~%" start packet-end packet)
                                     (values (list version id packet)
                                             packet-end)))
         (let ((length-type-id (aref binary (+ start 6))))
           (if (zerop length-type-id)
               (loop with subpackets-total-length
                       = (bit-smasher:bits->int (subseq binary (+ start 7) (+ start 7 15)))
-                        initially (format t "Aggregate packet w/ start ~a, total subpacket lenght of ~a~%"
-                                          start subpackets-total-length)
                     with end = (+ start 7 15)
                     while (/= (- end (+ start 7 15)) subpackets-total-length)
                     for (packet packet-end) = (multiple-value-list (parse-packet binary end))
                     collecting packet into packets
-                    do (format t "Just got packet ~a with end ~a~%" packet packet-end)
-                       (format t "The diff with start: ~a is ~a for an expected total subpacket length of ~a~%" start (- packet-end (+ start 7 15)) subpackets-total-length)
-                       (setf end packet-end)
+                    do (setf end packet-end)
                     finally
                        (return (values (list version id packets)
                                        end)))
               (loop with num-subpackets
                       = (bit-smasher:bits->int (subseq binary (+ start 7) (+ start 7 11)))
                     with end = (+ start 7 11)
-                      initially
-                         (format t "Aggregate packet w/ ~a subpackets~%"
-                                 num-subpackets)
                     for i below num-subpackets
                     for (packet packet-end) = (multiple-value-list (parse-packet binary end))
                     collecting packet into packets
@@ -75,29 +57,14 @@
                        (return (values (list version id packets)
                                        end))))))))
 
-(defun parse-packets (binary)
-  (parse-packet binary 0)
-  ;; (loop with packet-end = 0
-  ;;       with packets = nil
-  ;;       while (< packet-end (length binary))
-  ;;       do (progn (format t "Making root packet at ~a~%" packet-end)
-  ;;                 (multiple-value-bind (packet end) (parse-packet binary packet-end)
-  ;;                   (setf packets (append packets packet))
-  ;;                   (setf packet-end end)))
-  ;;       finally
-  ;;          (return packets))
-  )
-
-(defparameter *packets* (parse-packets *input*))
-
 (defun packet-valsum (packet)
-  (destructuring-bind (version id subpackets) packet
+  (destructuring-bind (version nil subpackets) packet
     (if (numberp subpackets)
         version
         (+ version (reduce #'+ (mapcar #'packet-valsum subpackets))))))
 
 (defun eval-packets (packet)
-  (destructuring-bind (version id subpackets) packet
+  (destructuring-bind (nil id subpackets) packet
     (ecase id
       (4 subpackets)
       (0 (reduce #'+ (mapcar #'eval-packets subpackets)))
@@ -107,3 +74,9 @@
       (5 (if (reduce #'> (mapcar #'eval-packets subpackets)) 1 0))
       (6 (if (reduce #'< (mapcar #'eval-packets subpackets)) 1 0))
       (7 (if (reduce #'= (mapcar #'eval-packets subpackets)) 1 0)))))
+
+(defun solve-7a ()
+  (packet-valsum (parse-packet *input* 0)))
+
+(defun solve-7b ()
+  (eval-packets (parse-packet *input* 0)))
