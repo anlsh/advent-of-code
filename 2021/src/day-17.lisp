04fc52032118a640c851b821bcaec42b2ba6f12b
Anish Moorthy <anlsh@protonmail.com>
2021-12-17 17:15:00 -0500

    foo

---
+++ 2021/src/day-17.lisp
@@ -0,0 +1,54 @@
+(uiop:define-package :advent/2021/src/day-17
+  (:use #:cl #:arrow-macros)
+  (:local-nicknames (#:alx #:alexandria) (#:pq #:priority-queue))
+  (:shadowing-import-from fset
+   :@))
+
+(in-package :advent/2021/src/day-17)
+
+(defparameter *input*
+  ;; (-<> (uiop:read-file-lines "../inputs/17.txt")
+  ;;   (cl-ppcre:register-groups-bind
+  ;;       (xmin xmax ymin ymax)
+  ;;       ("target area: x=(.*)\.\.(.*), y=(.*)\.\.(.*)") (car <>)
+  ;;     (list xmin xmax ymin ymax)))
+  (list 207 263 -115 -63)
+  )
+
+(defparameter *min-x* 207)
+(defparameter *max-x* 263)
+(defparameter *min-y* -115)
+(defparameter *max-y* -63)
+
+(defun min-x-velocity (min-x)
+  (loop for n from 0 while (< (* 1/2 n (1+ n))
+                              min-x)
+        finally (return n)))
+
+;; target area: x=20..30, y=-10..-5
+(defun step-probe (x y dx dy)
+  (list (+ x dx)
+        (+ y dy)
+        (* (signum dx) (1- (abs dx)))
+        (1- dy)))
+
+(defun velocity-hits-target (dx dy)
+  (loop with probe-state = (list 0 0 dx dy)
+        while t
+        do (setf probe-state (apply #'step-probe probe-state))
+           (destructuring-bind (x y dx dy) probe-state
+             (cond ((and (<= *min-x* x *max-x*)
+                         (<= *min-y* y *max-y*))
+                    (return-from velocity-hits-target t))
+                   ((or (< y *min-y*)
+                        (> x *max-x*)) (return-from velocity-hits-target nil))))))
+
+(let ((max-yvel (abs (1+ *min-y*))))
+  (defun solve-17a ()
+    (* 1/2 max-yvel (1+ max-yvel)))
+
+  (defun solve-17b ()
+    (loop for dx from (min-x-velocity *min-x*) upto *max-x*
+          appending (loop for dy from *min-y* upto max-yvel
+                          when (velocity-hits-target dx dy)
+                            collect (list dx dy)))))
