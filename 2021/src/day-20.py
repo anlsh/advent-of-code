915e9ed0a7b87e3a04debd92f310bd9a92337b51
Anish Moorthy <anlsh@protonmail.com>
2021-12-20 03:39:08 -0500

    Well, it was a noble effort based on padding

--- 2021/src/day-20.py
+++ 2021/src/day-20.py
@@ -12,18 +12,24 @@ def get_nborhood(set_points):
     nbor_sets = [set([tuple_add(root, off) for off in nbor_offs]) for root in set_points]
     return set().union(*nbor_sets)
 
-def step_picture(set_pts, key):
-    def get_val(loc):
-        if loc in set_pts:
+def step_picture(set_pts, key, fringe_zero):
+
+    close_nborhood = get_nborhood(set_pts)
+    far_nborhood = get_nborhood(close_nborhood)
+
+    def get_val(pos):
+        if pos in set_pts:
             return "1"
-        else:
+        elif pos in close_nborhood:
             return "0"
+        else:
+            return "0" if fringe_zero else "1"
 
     new_pts = set()
-    for pt in get_nborhood(set_pts):
-        bstr = "".join([get_val(tuple_add(pt, off)) for off in nbor_offs])
-        bnum = int(bstr, 2)
-        if key[bnum]:
+    for pt in far_nborhood:
+        s = "".join([get_val(tuple_add(pt, off)) for off in nbor_offs])
+
+        if key[int(s, 2)]:
             new_pts.add(pt)
 
     return new_pts
@@ -34,16 +40,17 @@ if __name__ == "__main__":
     set_pts = set()
 
     with open(fname) as f:
-        lines = f.readlines()
+        lines = [l.strip() for l in f.readlines()]
         for c in lines[0].strip():
             key.append(c == "#")
 
         for rnum, line in enumerate(lines[2:][::-1]):
+            line = line.strip()
             for cnum in range(len(line)):
                 if line[cnum] == "#":
                     set_pts.add((cnum, rnum))
 
-    for _ in range(2):
-        set_pts = step_picture(set_pts, key)
-
     print(f"Part A: {len(set_pts)}")
+    for i in range(2):
+        set_pts = step_picture(set_pts, key, (i % 2 == 0) and key[0])
+        print(f"Part A: {len(set_pts)}")
