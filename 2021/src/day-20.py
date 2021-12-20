b341ab6b6257ab4bb3fa6c7852efbe8bc06e03c9
Anish Moorthy <anlsh@protonmail.com>
2021-12-20 04:05:12 -0500

    Day 20

--- 2021/src/day-20.py
+++ 2021/src/day-20.py
@@ -5,36 +5,35 @@ nbor_offs = ((-1, 1), (0, 1), (1, 1),
              (-1, 0), (0, 0), (1, 0),
              (-1, -1), (0, -1), (1, -1))
 
-PADDING_SIZE = 5
+N_STEPS = 50
+PADDING_SIZE = N_STEPS + 5
 
 def tuple_add(x, y):
     return (x[0] + y[0], x[1] + y[1])
 
-# def get_nborhood(set_points):
-#     nbor_sets = [set([tuple_add(root, off) for off in nbor_offs]) for root in set_points]
-#     return set().union(*nbor_sets)
+def step_picture(picture, key, fringe_zero):
+    new_picture = [[0] * len(picture[0]) for _ in range(len(picture))]
 
-# def step_picture(set_pts, key, fringe_zero):
+    def get_numeric_val(pos):
+        if (0 <= pos[0] < len(picture[0])) and (0 <= pos[1] < len(picture)):
+            return "0" if picture[pos[1]][pos[0]] == "." else "1"
+        else:
+            return "0" if fringe_zero else "1"
 
-#     close_nborhood = get_nborhood(set_pts)
-#     far_nborhood = get_nborhood(close_nborhood)
+    for x in range(len(picture[0])):
+        for y in range(len(picture)):
+            s = "".join([get_numeric_val(tuple_add((x, y), off)) for off in nbor_offs])
+            new_picture[y][x] = "#" if key[int(s, 2)] else "."
 
-#     def get_val(pos):
-#         if pos in set_pts:
-#             return "1"
-#         elif pos in close_nborhood:
-#             return "0"
-#         else:
-#             return "0" if fringe_zero else "1"
+    return new_picture
 
-#     new_pts = set()
-#     for pt in far_nborhood:
-#         s = "".join([get_val(tuple_add(pt, off)) for off in nbor_offs])
-
-#         if key[int(s, 2)]:
-#             new_pts.add(pt)
-
-#     return new_pts
+def count_set_picture(picture):
+    c = 0
+    for row in picture:
+        for char in row:
+            if char == "#":
+                c += 1
+    return c
 
 if __name__ == "__main__":
     fname = sys.argv[1]
@@ -47,12 +46,14 @@ if __name__ == "__main__":
             key.append(c == "#")
 
         pic = []
-        for line in enumerate(lines[2:][::-1]):
+        for line in lines[2:][::-1]:
             pic.append(("." * PADDING_SIZE) + line.strip() + ("." * PADDING_SIZE))
 
         line_size = len(pic[0])
-        for _ in PADDING_SIZE:
+        for _ in range(PADDING_SIZE):
             pic = ["." * line_size] + pic + ["." * line_size]
 
-    for i in range(2):
-        pic = step_picture(pic, key, (i % 2 == 0) and key[0])
+    print(f"Beginning count {count_set_picture(pic)}")
+    for i in range(N_STEPS):
+        pic = step_picture(pic, key, (not key[0]) or (i % 2 == 0))
+        print(f"Then {count_set_picture(pic)}")
