79865fc8a6f680159aa542880bdff9bde85ea9b6
Anish Moorthy <anlsh@protonmail.com>
2021-12-24 20:46:17 -0500

    Oh god we're looping through it all

--- 2021/src/day-24.py
+++ 2021/src/day-24.py
@@ -1,4 +1,5 @@
 import sys
+import itertools
 
 LETS = "xyzw"
 
@@ -7,6 +8,8 @@ MUL_KEY = "*"
 DIV_KEY = "/"
 MOD_KEY = "%"
 EQL_KEY = "=="
+
+NUMS = (1,2,3,4,5,6,7,8,9)
 
 def get_ops(fname):
     with open(fname) as f:
@@ -79,8 +82,47 @@ def state_from_ops(ops):
 
     return state
 
+def run_program(ops, in_num_tuple):
+    state = {"x": 0, "y": 0, "z": 0, "w": 0}
+    i = 0
+    for op in ops:
+        if op[0] == "inp":
+            state[op[1]] = in_num_tuple[i]
+            i += 1
+            continue
+        place = op[1]
+        arg1 = state[op[1]]
+        arg2 = state[op[2]] if op[2] in LETS else int(op[2])
+        if op[0] == "add":
+            state[place] = arg1 + arg2
+        elif op[0] == "mul":
+            state[place] = arg1 * arg2
+        elif op[0] == "div":
+            if arg2 == 0:
+                raise RuntimeError("Dividing by zero!")
+            state[place] = arg1 // arg2
+        elif op[0] == "mod":
+            if arg2 == 0:
+                raise RuntimeError("Modding by zero!")
+            state[place] = arg1 % arg2
+        elif op[0] == "eql":
+            state[place] = int(arg1 == arg2)
+
+    return state
+
 if __name__ == "__main__":
-    trees = state_from_ops(get_ops(sys.argv[1]))
-    inp = [int(c) for c in sys.argv[2]]
-    final_state = {t: eval_tree(trees[t], inp) for t in trees}
-    print(final_state)
+    # trees = state_from_ops(get_ops(sys.argv[1]))
+    # inp = [int(c) for c in sys.argv[2]]
+    # final_state = {t: eval_tree(trees[t], inp) for t in trees}
+    # print(final_state)
+    ops = get_ops(sys.argv[1])
+
+    i = 0
+    for in_number in itertools.product(NUMS[::-1], repeat=int(sys.argv[2])):
+        z_state = run_program(ops, in_number)["z"]
+        nstr = "".join([str(i) for i in in_number])
+        i += 1
+        if z_state == 0:
+            print(f"Accepted {nstr}!")
+            exit()
+    print(i)
